ff7c12c5b5e5c1ac316b9b6b026f4c45743d928c
Jared Brown <jaredbrown1@hotmail.com>
2020-07-22 22:34:21 -0700

    2 stack tests passing

--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,110 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+# STACK OF PLATES AT A BUFFET! THE PLATE AT THE BOTTOM OF THE STACK WAS THE FIRST ONE ADDED
+
+# WITH LINKED LIST
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
 class Stack:
     def __init__(self):
+        self.head = None
         self.size = 0
-        # self.storage = ?
+
+    def push(self, data):
+        if self.head is None:
+            self.head = Node(data)
+        else:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
 
     def __len__(self):
-        pass
-
-    def push(self, value):
-        pass
+        return self.size
 
     def pop(self):
-        pass
+        if self.head is None:
+            return None
+        else:
+            popped = self.head.data
+            self.head = self.head.next
+            return popped
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = LinkedList()
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def push(self, value):
+#         self.storage.append(value)
+
+#     def pop(self):
+#         return self.storage.pop()
+
+# WITH ARRAY
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def push(self, value):
+#         self.storage.append(value)
+
+#     def pop(self):
+#         return self.storage.pop()
+
+# class Node:
+
+#     def __init__(self):
+#         self.data = None
+#         self.next = None
+
+#     def getData(self):
+#         return self.data
+
+#     def getNext(self):
+#         return self.next
+
+#     def setData(self, newdata):
+#         self.data = newdata
+
+#     def setNext(self, newnext):
+#         self.next = newnext
+
+
+# class Stack:
+
+#     def __init__(self):
+#         self.head = None
+
+#     def __len__(self):
+#         temp = Node()
+#         temp = self.getData()
+#         return len(self.head)
+
+#     def push(self, data):
+
+#         temp = Node()
+#         temp.setData(data)
+#         temp.setNext(self.head)
+#         self.head = temp
+
+#     def pop(self):
+
+#         temp = Node()
+#         temp = self.getData()
+#         self.head = self.head.getNext()
+#         return temp
