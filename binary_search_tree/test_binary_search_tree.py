d0076a598528f07d29e3ca71b99522ced42e51a3
Jared Brown <jaredbrown1@hotmail.com>
2020-09-18 09:45:57 -0700

    restart for flex

--- binary_search_tree/test_binary_search_tree.py
+++ binary_search_tree/test_binary_search_tree.py
@@ -4,7 +4,6 @@ import sys
 import io
 from binary_search_tree import BSTNode
 
-
 class BinarySearchTreeTests(unittest.TestCase):
     def setUp(self):
         self.bst = BSTNode(5)
@@ -16,7 +15,7 @@ class BinarySearchTreeTests(unittest.TestCase):
         self.bst.insert(6)
         self.assertEqual(self.bst.left.right.value, 3)
         self.assertEqual(self.bst.right.left.value, 6)
-
+        
     def test_handle_dupe_insert(self):
         self.bst2 = BSTNode(1)
         self.bst2.insert(1)
@@ -39,7 +38,7 @@ class BinarySearchTreeTests(unittest.TestCase):
 
     def test_for_each(self):
         arr = []
-        def cb(x): return arr.append(x)
+        cb = lambda x: arr.append(x)
 
         v1 = random.randint(1, 101)
         v2 = random.randint(1, 101)
@@ -106,7 +105,6 @@ class BinarySearchTreeTests(unittest.TestCase):
         self.assertEqual(output, "2\n4\n3\n6\n7\n5\n8\n1\n")
 
         sys.stdout = stdout_  # Restore stdout
-
 
 if __name__ == '__main__':
     unittest.main()
